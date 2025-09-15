e28e4fe6cf4df7be7fe1d2dceec191b1253b16ef
Carlos Cunha <carlosrubim13@gmail.com>
2025-09-15 13:57:34 -0300

    Finalização do ex. 1179 BeeCrowd

--- beecrowd/1179.js
+++ beecrowd/1179.js
@@ -1,30 +1,32 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
+var lines = input.split('\n').filter(l => l !== ''); // remove linhas vazias
 
 let par = [];
 let impar = [];
 
-for (let i = 0; i < lines.length; i++) {
-  if (parseInt(lines[i]) % 2 === 0) {
-    par.push(parseInt(lines[i]));
-    if (par.length === 5) { // encheu
-      for (let i = 0; i < 5; i++) {
-        console.log(`par[${i}] = ${par[i]}`);
+for (let i = 0; i < 15; i++) { // ler exatamente 15 números
+  let num = parseInt(lines[i]);
+
+  if (num % 2 === 0) {
+    par.push(num);
+    if (par.length === 5) {
+      for (let j = 0; j < 5; j++) {
+        console.log(`par[${j}] = ${par[j]}`);
       }
-      par = []; // esvazia
+      par = [];
     }
   } else {
-    impar.push(parseInt(lines[i]));
-    if (impar.length === 5) { // encheu
-      for (let i = 0; i < 5; i++) {
-        console.log(`impar[${i}] = ${impar[i]}`);
+    impar.push(num);
+    if (impar.length === 5) {
+      for (let j = 0; j < 5; j++) {
+        console.log(`impar[${j}] = ${impar[j]}`);
       }
-      impar = []; // esvazia
+      impar = [];
     }
   }
 }
 
-// Depois da leitura, imprime o que restou
+// Imprime o que restou nos vetores
 for (let i = 0; i < impar.length; i++) {
   console.log(`impar[${i}] = ${impar[i]}`);
 }
